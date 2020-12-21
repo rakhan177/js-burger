d9a2385bba3cd92dfa9c212b44fb5e301efca931
Matteo <metboolean@gmail.com>
2020-12-21 16:46:08 +0100

    elimino else piu controllo checked

--- js/script.js
+++ js/script.js
@@ -15,22 +15,27 @@ var total = 50;
 button.addEventListener('click', function() {
   // controllo che l' utente abbia inserito il nome e lo richiamo in caso contrario
   if (nameBurger.value === '') {
-    alert('Inserire nome burger');
-  }/*altrimenti avvio le operazioni*/else{
-    // ad ogni click total torna a 50 altrimenti continuerebba ad aumentare
-    total = 50;
-    // creo ciclo for per verificare i checkbox selezionati dall' utente
-    for (var i = 0; i < checkElements.length; i++) {
-      if (checkElements[i].checked) {
-        // ogni elemento checkato counter aumenta di 1
-        counter += 1;
-        // stabilisco il nuovo total sommandolo al parseInt del valore html dei checkElements
-        total += parseInt(checkElements[i].value);
-      }
-    }
-
-    price.innerHTML = total +" "+ 'euro';
+    alert('Inserire nome burger.');
   }
+  /*altrimenti avvio le operazioni*/
+  // ad ogni click total torna a 50 altrimenti continuerebba ad aumentare
+  total = 50;
+  // creo ciclo for per verificare i checkbox selezionati dall' utente
+  for (var i = 0; i < checkElements.length; i++) {
+    if (checkElements[i].checked) {
+      // ad ogni elemento checkato counter aumenta di 1
+      counter += 1;
+      // stabilisco il nuovo total sommandolo al parseInt del valore html i checkElements
+      total += parseInt(checkElements[i].value);
+    }
+  }
+  // controllo che l utente abbia inserito almeno due ingredienti,se così non fosse lo richiamo
+  if (counter < 2) {
+    alert('La prego di selezionare almeno due ingredienti.');
+  }
+  // stampo all' utente il prezzo finale
+  price.innerHTML = total +" "+ 'euro';
+  
 
 
 
